b953a99431d8a0cc159f5ecfd4dfa9b6b12a024f
Diego Costantino <diegocstn@gmail.com>
2015-08-28 11:49:44 +0200

    Test for more building conversion

--- core/src/test/java/APIFactoryTest.java
+++ core/src/test/java/APIFactoryTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import it.unimi.unimiplaces.api.APIFactory;
+import it.unimi.unimiplaces.model.BaseEntity;
 import it.unimi.unimiplaces.model.Building;
 
 
@@ -47,5 +49,26 @@ public class APIFactoryTest {
 
         Assert.assertNotNull(other);
         Assert.assertEquals( actual,other );
+    }
+
+    @Test
+    public void TestFactoryWithThreeBuildings(){
+        String jsonBuilding = readFixtureDataFromFile("building_single.json");
+        ArrayList<BaseEntity> actualBuildings = new ArrayList<>();
+
+        Building b1 = new Building("11020","Festa Del Perdono","via Festa del Perdono, 3, Milano, 20122");
+        b1.setCoordinates(9.194568, 45.460998);
+
+        Building b2 = new Building("11120","Sant'Antonio","via Sant'Antonio, 5, Milano, 20122");
+        b2.setCoordinates(9.193712, 45.461615);
+
+        Building b3 = new Building("11220","Santa Sofia","Via Santa Sofia, 9/11, Milano, 20122");
+        b3.setCoordinates(9.191074, 45.456215);
+
+        actualBuildings.add(b1);
+        actualBuildings.add( b2 );
+        actualBuildings.add( b3 );
+
+        ArrayList<BaseEntity> expectedBuildings = apiFactory.makeBuildingsFromJSON(jsonBuilding);
     }
 }
