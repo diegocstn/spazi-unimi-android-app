ac10bde39fc5d2c3c007d711c1ac3678e9a1cd39
Diego Costantino <diegocstn@gmail.com>
2015-09-01 21:29:22 +0200

    fix APIDelegateInterface return type

--- core/src/main/java/it/unimi/unimiplaces/core/api/APIDelegateInterface.java
+++ core/src/main/java/it/unimi/unimiplaces/core/api/APIDelegateInterface.java
@@ -1,10 +1,10 @@
 package it.unimi.unimiplaces.core.api;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import it.unimi.unimiplaces.core.model.BaseEntity;
 
 public interface APIDelegateInterface {
     void apiRequestStart();
-    void apiRequestEnd(ArrayList<BaseEntity> results);
+    void apiRequestEnd(List<BaseEntity> results);
 }
