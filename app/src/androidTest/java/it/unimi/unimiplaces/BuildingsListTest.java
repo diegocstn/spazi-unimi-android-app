2b54d6cf19fc3befbb976f8fa81100ca89214ebd
Diego Costantino <diegocstn@gmail.com>
2015-09-25 11:28:48 +0200

    BuildingsListFragment first test

---
+++ app/src/androidTest/java/it/unimi/unimiplaces/BuildingsListTest.java
@@ -0,0 +1,43 @@
+package it.unimi.unimiplaces;
+
+import android.widget.ListView;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unimi.unimiplaces.core.model.BaseEntity;
+import it.unimi.unimiplaces.core.model.Building;
+import it.unimi.unimiplaces.fragments.BuildingsListFragment;
+
+/**
+ * BuildingsListFragment test class
+ */
+
+public class BuildingsListTest extends FragmentTest{
+
+    private ListView listView;
+    private BuildingsListFragment buildingsListFragment;
+
+    @Test
+    public void testThreeBuildings(){
+        buildingsListFragment = new BuildingsListFragment();
+        this.prepareActivityWithFragment(buildingsListFragment);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<BaseEntity> buildings = new ArrayList<>();
+                buildings.add(new Building("0001","Building1","Building address 1"));
+                buildings.add(new Building("0002", "Building2", "Building address 2"));
+                buildings.add(new Building("0003", "Building3", "Building address 3"));
+                buildingsListFragment.setModel(activity, buildings);
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        listView = (ListView) buildingsListFragment.getView().findViewById(android.R.id.list);
+        assertEquals(3, listView.getAdapter().getCount());
+    }
+
+}
