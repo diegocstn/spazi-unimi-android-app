09da2d02b71a54cba5efa3f89e5e5fa4a6acfbed
Diego Costantino <diegocstn@gmail.com>
2015-09-25 11:32:14 +0200

    refactoring tests

--- app/src/androidTest/java/it/unimi/unimiplaces/BuildingsListTest.java
+++ app/src/androidTest/java/it/unimi/unimiplaces/BuildingsListTest.java
@@ -20,24 +20,36 @@ public class BuildingsListTest extends FragmentTest{
     private ListView listView;
     private BuildingsListFragment buildingsListFragment;
 
-    @Test
-    public void testThreeBuildings(){
-        buildingsListFragment = new BuildingsListFragment();
-        this.prepareActivityWithFragment(buildingsListFragment);
+    public void setBuildingsAdapter(List<BaseEntity> buildings){
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 List<BaseEntity> buildings = new ArrayList<>();
-                buildings.add(new Building("0001","Building1","Building address 1"));
+                buildings.add(new Building("0001", "Building1", "Building address 1"));
                 buildings.add(new Building("0002", "Building2", "Building address 2"));
                 buildings.add(new Building("0003", "Building3", "Building address 3"));
                 buildingsListFragment.setModel(activity, buildings);
             }
         });
+        getInstrumentation().waitForIdleSync();
+    }
+
+    @Test
+    public void testThreeBuildings(){
+        buildingsListFragment = new BuildingsListFragment();
+        this.prepareActivityWithFragment(buildingsListFragment);
+
+        List<BaseEntity> buildings = new ArrayList<>();
+        buildings.add(new Building("0001", "Building1", "Building address 1"));
+        buildings.add(new Building("0002", "Building2", "Building address 2"));
+        buildings.add(new Building("0003", "Building3", "Building address 3"));
+
+        setBuildingsAdapter(buildings);
 
         getInstrumentation().waitForIdleSync();
         listView = (ListView) buildingsListFragment.getView().findViewById(android.R.id.list);
         assertEquals(3, listView.getAdapter().getCount());
     }
+    
 
 }
