38cbd543d5e528e8112fca27303712b86978bb2c
Diego Costantino <diegocstn@gmail.com>
2015-10-12 10:21:48 +0200

    RoomDetailActivity backbone

---
+++ app/src/main/java/it/unimi/unimiplaces/activities/RoomDetailActivity.java
@@ -0,0 +1,14 @@
+package it.unimi.unimiplaces.activities;
+
+import android.os.Bundle;
+
+import it.unimi.unimiplaces.R;
+
+public class RoomDetailActivity extends AppDetailSectionActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_detail);
+    }
+}
