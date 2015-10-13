158e6e3b1d98dbb05aa4066469f0f10cc3b6b85d
Diego Costantino <diegocstn@gmail.com>
2015-10-13 10:49:17 +0200

    loading UI FloorMapView

--- app/src/main/java/it/unimi/unimiplaces/views/FloorMapView.java
+++ app/src/main/java/it/unimi/unimiplaces/views/FloorMapView.java
@@ -5,6 +5,7 @@ import android.util.AttributeSet;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import it.unimi.unimiplaces.R;
 
@@ -13,6 +14,7 @@ import it.unimi.unimiplaces.R;
  */
 public class FloorMapView extends RelativeLayout {
 
+    private TextView loadingTextView;
     private WebView webView;
     private final String HTMLfileURL = "file:///android_asset/floormap.html";
 
@@ -28,7 +30,10 @@ public class FloorMapView extends RelativeLayout {
 
     private void init(){
         inflate(getContext(), R.layout.view_floor_map,this);
-        this.webView = (WebView) findViewById(R.id.webview);
+
+        this.loadingTextView    = (TextView) findViewById(R.id.webview_loading);
+        this.webView            = (WebView) findViewById(R.id.webview);
+
         this.webView.getSettings().setJavaScriptEnabled(true);
         this.webView.getSettings().getAllowFileAccessFromFileURLs();
         this.webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
@@ -39,6 +44,11 @@ public class FloorMapView extends RelativeLayout {
         this.webView.loadUrl(HTMLfileURL);
     }
 
+    private void pageLoadingFinished(){
+        this.loadingTextView.setVisibility(GONE);
+        this.webView.setVisibility(VISIBLE);
+    }
+
     public void loadFloorMap(String svgURL){
         final String script = String.format("svgFloor.init(\"%s\")",svgURL);
         this.webView.setWebViewClient(new WebViewClient() {
@@ -46,6 +56,7 @@ public class FloorMapView extends RelativeLayout {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 webView.evaluateJavascript(script, null);
+                pageLoadingFinished();
             }
         });
     }
@@ -57,6 +68,7 @@ public class FloorMapView extends RelativeLayout {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 webView.evaluateJavascript(script, null);
+                pageLoadingFinished();
             }
         });
 
