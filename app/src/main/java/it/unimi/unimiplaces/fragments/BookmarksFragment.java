ab6e2bc09c87f732cf2d1d9b6827d7a535896e75
Diego Costantino <diegocstn@gmail.com>
2015-10-15 15:58:55 +0200

    bookmarks fragment init

--- app/src/main/java/it/unimi/unimiplaces/fragments/BookmarksFragment.java
+++ app/src/main/java/it/unimi/unimiplaces/fragments/BookmarksFragment.java
@@ -1,6 +1,7 @@
 package it.unimi.unimiplaces.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.InflateException;
@@ -9,7 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unimi.unimiplaces.Bookmark;
@@ -25,7 +28,7 @@ public class BookmarksFragment extends Fragment implements BookmarksViewInterfac
     private final String LOG_TAG = "BOOKMARKS";
     private View view;
     private ListView listView;
-    private ArrayAdapter arrayAdapter;
+    private BookmarksListAdapter arrayAdapter;
 
     public static BookmarksFragment newInstance() {
         BookmarksFragment fragment = new BookmarksFragment();
@@ -57,13 +60,13 @@ public class BookmarksFragment extends Fragment implements BookmarksViewInterfac
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         listView = (ListView) view.findViewById(R.id.bookmarks_list);
+        listView.setAdapter(this.arrayAdapter);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS));*/
+        this.arrayAdapter = new BookmarksListAdapter(getActivity(),new ArrayList<Bookmark>());
     }
 
     @Override
@@ -78,16 +81,57 @@ public class BookmarksFragment extends Fragment implements BookmarksViewInterfac
 
     @Override
     public void setModel(List<Bookmark> bookmarks) {
-
+        this.arrayAdapter.clear();
+        this.arrayAdapter.addAll(bookmarks);
+        this.arrayAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void showBookmarksList() {
-
+        this.listView.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void showNoResults() {
 
+    }
+
+    private class BookmarksListAdapter extends ArrayAdapter<Bookmark>{
+        private final Context context;
+        private final List<Bookmark> bookmarks;
+
+        private class BookmarkViewHolder{
+            public TextView bookmarkTitle;
+        }
+
+        public BookmarksListAdapter(Context context,List<Bookmark> bookmarks){
+            super( context, R.layout.bookmarks_list_item , bookmarks );
+            this.context    = context;
+            this.bookmarks  = bookmarks;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup group) {
+            View bookmarkView = convertView;
+
+            // reuse view
+            if( bookmarkView==null ){
+                LayoutInflater inflater = LayoutInflater.from(context);
+                bookmarkView = inflater.inflate(R.layout.bookmarks_list_item,null);
+
+                // create BookmarkViewHolder in order to avoid searching
+                // resources within XML and speeds up rendering
+                BookmarkViewHolder holder = new BookmarkViewHolder();
+                holder.bookmarkTitle     = (TextView) bookmarkView.findViewById(R.id.bookmark_title);
+                bookmarkView.setTag(holder);
+            }
+
+            // fill with building data
+            BookmarkViewHolder holder = (BookmarkViewHolder) bookmarkView.getTag();
+            Bookmark bookmark = this.bookmarks.get(position);
+            holder.bookmarkTitle.setText(bookmark.title);
+
+            return bookmarkView;
+        }
     }
 }
